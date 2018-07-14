a026b1ddf3af4cbea3f377198376d18be86f795b
Deni Cho <dencho.12@gmail.com>
2018-07-15 01:41:54 +0700

    Setup Postgres

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,18 +1,27 @@
+
+import os
+
+
 class BaseConfig:
     """Base Configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     pass
 
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
     pass
 
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     pass
